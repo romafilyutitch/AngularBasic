092a15a25a6d68217dafc0b2344dd80312a35248
Roman Filiyutich <roman.filiyutich@sap.com>
2022-11-06 03:18:26 +0300

    7. (Extra task) create an attribute directive to toggle password

---
+++ src/app/shared/directives/password-toggle.directive.ts
@@ -0,0 +1,19 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appPasswordToggle]',
+  exportAs: 'passwordToggle'
+})
+export class PasswordToggleDirective {
+
+  showPassword: boolean = false;
+
+  constructor() { 
+    
+  }
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent, ConfirmModalComponent, CourseCardComponent, CourseListComponent, DurationPipe, HeaderComponent, InfoComponent, CreationDatePipe, SearchComponent } from './components';
+import { ButtonComponent, ConfirmModalComponent, CourseCardComponent, CourseListComponent, HeaderComponent, InfoComponent, SearchComponent } from './components';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EmailValidatorDirective } from './directives/email-validator.directive';
 import { FormsModule } from '@angular/forms';
+import { PasswordToggleDirective } from './directives/password-toggle.directive';
+import { CreationDatePipe } from './pipes/creation-date.pipe';
+import { DurationPipe } from './pipes/duration.pipe';
 
 const components = [
   HeaderComponent, 
   ButtonComponent, 
   InfoComponent,
   CourseCardComponent,
-  DurationPipe,
-  CreationDatePipe,
   CourseListComponent,
   ConfirmModalComponent,
-  EmailValidatorDirective,
-  SearchComponent
+  SearchComponent, 
 ]
 
 @NgModule({
-  declarations: components,
-  exports: components,
+  declarations: [...components, DurationPipe, CreationDatePipe, EmailValidatorDirective, PasswordToggleDirective],
+  exports: [...components, DurationPipe, CreationDatePipe, EmailValidatorDirective, PasswordToggleDirective],
   imports: [
     CommonModule, FontAwesomeModule, FormsModule
   ]
